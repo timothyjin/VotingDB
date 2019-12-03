19824a8a04e32f88375dc4d124d8e6e2c7d7b3af
timothyjin <tjin316@gmail.com>
2019-12-03 01:38:06 -0500

    Add main loop and basic design of command line interface

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
+import sys
+import inspect
 import getpass
+import mysql
 import mysql.connector as driver
 
 user = 'root'
@@ -8,43 +11,88 @@ db_name = 'voting_db'
 connection = driver.connect(user=user, password=passwd, database=db_name)
 cursor = connection.cursor()
 
-# SQL command to create a table in the database
-sql_command = """CREATE TABLE IF NOT EXISTS Election(
-year INTEGER PRIMARY KEY,
-month INTEGER,
-day INTEGER);"""
-
-# execute the statement
-cursor.execute(sql_command)
-
-# SQL command to insert the data in the table
-sql_command = """INSERT INTO Election VALUES (2018, 11, 6);"""
-cursor.execute(sql_command)
-
-# another SQL command to insert the data in the table
-sql_command = """INSERT INTO Election VALUES (2016, 11, 8);"""
-cursor.execute(sql_command)
-
-# another SQL command to retrieve data in the table
-sql_command = """SELECT * FROM Election;"""
-cursor.execute(sql_command)
-for y, m, d in cursor:
-    print(y, m, d)
-
 
 def Find_Candidates(position, year):
-    sql_command = f"""SELECT Candidate.SSN, Candidate.name, Candidates.party FROM Candidate NATURAL JOIN RunsOn WHERE RunsOn.position  = {position} AND RunsOn.year = {year}"""
-    candidates = cursor.execute(sql_command)
-    return candidates
-
-def Create_Voter(SSN, name, birthday, gender, ethnicity, income, party):
-    sql_command = f"""INSERT INTO Voter VALUES ({SSN}, {name}, {birthday}, {gender}, {ethnicity}, {income}, {party})"""
+    sql_command = f"""SELECT Candidate.SSN, Candidate.name_first, Candidate.name_last, Candidate.party FROM Candidate NATURAL JOIN RunsOn WHERE RunsOn.position = "{position}" AND RunsOn.year = {year};"""
     cursor.execute(sql_command)
-    print(f"Voter {name} has been created")
+    return cursor
 
-# To save the changes in the files. Never skip this.
-# If we skip this, nothing will be saved in the database.
-connection.commit()
+
+def Create_Voter(SSN, first_name, middle_name, last_name, birthday, gender, ethnicity, income, party):
+    sql_command = f"""INSERT INTO Voter VALUES ("{SSN}", "{first_name}", "{middle_name}", "{last_name}", "{birthday}", "{gender}", "{ethnicity}", {income}, "{party}")"""
+    try:
+        cursor.execute(sql_command)
+        print(f"Voter {name} has been created")
+    except mysql.connector.errors.ProgrammingError as e:
+        print(e)
+
+
+def Create_Ballot(voter_SSN, ID, position, year, submission_time, absentee, candidate_SSN):
+    sql_command = f"""INSERT_INTO Ballot VALUES ({ID}, "{position}", {year}, {submission_time}, {absentee})"""
+    cursor.execute(sql_command)
+    print(f"Ballot with ID {ID} has been created")
+
+
+def Participation_Rate(position, year, number, state):
+    sql_command = f"""SELECT COUNT(*) / (SELECT MAX(d.population) FROM District d WHERE d.year = {year} AND d.number = {number} AND d.state = "{state}")
+    FROM LocatedIn l NATURAL JOIN Ballot b
+    WHERE l.position = "{position}" AND l.year = {year} AND l.number = {number} AND l.state = "{state}";"""
+    cursor.execute(sql_command)
+    return cursor
+
+
+def Custom_Query():
+    sql_command = str(input('Query: '))
+    cursor.execute(sql_command)
+    return cursor
+
+
+def attributes(method):
+    parameters = inspect.getfullargspec(method)[0]
+    values = []
+    for p in parameters:
+        attr = input(p + ': ')
+        values.append(attr)
+    return values
+
+
+menu = """
+1. Insert voter
+2. Insert ballot
+3. Find candidates
+4. Find election winner
+5. Get participation rate
+6. Custom SQL query
+0. Exit
+> """
+
+options = {
+    1: Create_Voter,
+    2: Create_Ballot,
+    3: Find_Candidates, #('Senator from Missouri', 2020)
+    4: None, # Find election winner
+    5: Participation_Rate, #('Ohio 3rd District Representative', 2020, 3, 'OH'):
+    6: Custom_Query
+}
+
+while True:
+    choice = int(input(menu))
+
+    if choice == 0:
+        break
+    elif choice not in options:
+        print('Not a valid option.')
+        continue
+
+    method = options[choice]
+    attribute_values = attributes(method)
+    result = getattr(sys.modules[__name__], method.__name__)(*attribute_values)
+    if result:
+        for t in result:
+            print(t)
+    # TODO this only needs to be called after inserting records
+    # connection.commit()
+    print('------------------------------')
 
 # close the connection
 connection.close()
