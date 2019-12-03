5c1e84bae2c0457d73a4d3bb36b7e51d3f32d74b
Philip Woolley <philipgmwoolley@gmail.com>
2019-12-02 23:09:15 -0500

    removing webapp

--- backend/bootstrap.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export FLASK_APP=./src/main.py
-conda.bat activate Databases
-flask run -h 0.0.0.0
--- frontend/src/app/elections/election.model.ts
+++
@@ -1,9 +0,0 @@
-export class Election {
-	constructor(
-		public position: String,
-		public year: Number,
-		public month: Number,
-		public day: Number,
-	){}
-	
-}
