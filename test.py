90c8016e746205955df12e3fbc021a4521138e6a
timothyjin <tjin316@gmail.com>
2019-12-03 02:00:24 -0500

    Add really sketchy exception handling

--- test.py
+++ test.py
@@ -14,45 +14,56 @@ cursor = connection.cursor()
 
 def Find_Candidates(position, year):
     sql_command = f"""SELECT Candidate.SSN, Candidate.name_first, Candidate.name_last, Candidate.party FROM Candidate NATURAL JOIN RunsOn WHERE RunsOn.position = "{position}" AND RunsOn.year = {year};"""
-    cursor.execute(sql_command)
+    execute(sql_command)
     return cursor
 
 
 def Create_Voter(SSN, first_name, middle_name, last_name, birthday, gender, ethnicity, income, party):
     sql_command = f"""INSERT INTO Voter VALUES ("{SSN}", "{first_name}", "{middle_name}", "{last_name}", "{birthday}", "{gender}", "{ethnicity}", {income}, "{party}")"""
-    try:
-        cursor.execute(sql_command)
-        print(f"Voter {name} has been created")
-    except mysql.connector.errors.ProgrammingError as e:
-        print(e)
+    status = execute(sql_command)
+    if status:
+        print(f"Voter with SSN {SSN} has been created")
 
 
 def Create_Ballot(voter_SSN, ID, position, year, submission_time, absentee, candidate_SSN):
     sql_command = f"""INSERT_INTO Ballot VALUES ({ID}, "{position}", {year}, {submission_time}, {absentee})"""
-    cursor.execute(sql_command)
-    print(f"Ballot with ID {ID} has been created")
+    status = execute(sql_command)
+    if status:
+        print(f"Ballot with ID {ID} has been created")
 
 
 def Participation_Rate(position, year, number, state):
     sql_command = f"""SELECT COUNT(*) / (SELECT MAX(d.population) FROM District d WHERE d.year = {year} AND d.number = {number} AND d.state = "{state}")
     FROM LocatedIn l NATURAL JOIN Ballot b
     WHERE l.position = "{position}" AND l.year = {year} AND l.number = {number} AND l.state = "{state}";"""
-    cursor.execute(sql_command)
+    execute(sql_command)
     return cursor
 
 
 def Custom_Query():
     sql_command = str(input('Query: '))
-    cursor.execute(sql_command)
+    execute(sql_command)
     return cursor
 
 
-def attributes(method):
+def execute(command):
+    try:
+        cursor.execute(command)
+        return True
+    except Exception as e:
+        print(e)
+    return False
+
+
+def attribute_values(method):
     parameters = inspect.getfullargspec(method)[0]
     values = []
     for p in parameters:
         attr = input(p + ': ')
-        values.append(attr)
+        if attr:
+            values.append(attr)
+        else:
+            values.append(None)
     return values
 
 
@@ -75,6 +86,7 @@ options = {
     6: Custom_Query
 }
 
+# Main program loop
 while True:
     choice = int(input(menu))
 
@@ -84,15 +96,15 @@ while True:
         print('Not a valid option.')
         continue
 
+    # Call corresponding method and print results if applicable
     method = options[choice]
-    attribute_values = attributes(method)
-    result = getattr(sys.modules[__name__], method.__name__)(*attribute_values)
+    result = getattr(sys.modules[__name__], method.__name__)(*attribute_values(method))
     if result:
         for t in result:
             print(t)
-    # TODO this only needs to be called after inserting records
-    # connection.commit()
     print('------------------------------')
 
 # close the connection
+connection.commit()
 connection.close()
+print('Bye')
